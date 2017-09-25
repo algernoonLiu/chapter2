25cac16a2b3835fe505aabd0819ae8025a3bcf5d
algernoon <865926238@qq.com>
2017-09-25 18:14:13 +0800

    ddd

--- src/main/java/org/smart4j/chapter2/controller/CustomerServlet.java
+++ src/main/java/org/smart4j/chapter2/controller/CustomerServlet.java
@@ -1,11 +1,15 @@
 package org.smart4j.chapter2.controller;
 
-;import javax.servlet.ServletException;
+;import org.smart4j.chapter2.model.Customer;
+import org.smart4j.chapter2.service.CustomerService;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * http://www.otvcloud.com/
@@ -20,9 +24,18 @@ import java.io.IOException;
 @WebServlet("/customer")
 public class CustomerServlet extends HttpServlet {
 
+    private CustomerService customerService;
+
+    @Override
+    public void init() throws ServletException {
+        customerService = new CustomerService();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        List<Customer> customerList = customerService.getCustomerList();
+        req.setAttribute("customerList", customerList);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req, resp);
     }
 
     @Override
